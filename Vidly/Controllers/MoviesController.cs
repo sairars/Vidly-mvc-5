edf52720fd9904a3846b0734d82ac1890889d11b
Saira Rashid <saira.rashid1216@gmail.com>
2023-01-29 15:40:22 -0500

    Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,9 +10,15 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
-            return View();
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
         // GET: Movies/Random
@@ -23,6 +29,12 @@ namespace Vidly.Controllers
             //return Content("Hello World");
             //return HttpNotFound();
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy="name"};
+        }
+
+        // GET: Movies/Edit/1
+        public ActionResult Edit(int id)
+        {
+            return Content("id: " + id);
         }
     }
 }
