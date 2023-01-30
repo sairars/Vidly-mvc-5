d5fd54589d603dbce9670a82b3e8ab4c87e4e229
Saira Rashid <saira.rashid1216@gmail.com>
2023-01-30 14:37:05 -0500

    Add dateofbirth and customer details page to the application

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -14,6 +14,8 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        public DateTime? DateOfBirth { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
         
         public byte MembershipTypeId { get; set; }
