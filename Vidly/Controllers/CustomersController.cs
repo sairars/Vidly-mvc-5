0c59d261bbf6924983f4d0ab64bb5b6301e683ac
Saira Rashid <saira.rashid1216@gmail.com>
2023-01-30 10:13:53 -0500

    Eager load membership data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
+
 
 namespace Vidly.Controllers
 {
@@ -24,7 +26,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
