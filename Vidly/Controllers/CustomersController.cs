127ef0c85c2e70d8fd48abcc0fd2f296d9883b09
Saira Rashid <saira.rashid1216@gmail.com>
2023-01-31 12:29:53 -0500

    Model Binding and Saving the customer in DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -58,5 +58,14 @@ namespace Vidly.Controllers
 
             return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index", "Customers");
+        }
     }
 }
